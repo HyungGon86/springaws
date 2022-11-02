c76c82fe110b4919b4e111e412313abb93c4600d
HyungGon <kgh198621@naver.com>
2022-11-02 22:08:59 +0900

    Initial commit

---
+++ src/test/java/study/springaws/SpringAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
