a32334ac62b0c575ef6473745364668f1338962c
HyungGon <kgh198621@naver.com>
2022-12-08 17:51:45 +0900

    테스트 커밋푸시

--- src/test/java/study/springaws/SpringAwsApplicationTests.java
+++ src/test/java/study/springaws/SpringAwsApplicationTests.java
@@ -1,13 +1,9 @@
 package study.springaws;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringAwsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
