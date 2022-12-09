85bd7a11193623b0cefd88bfd00d7ae811cfaad9
HyungGon <kgh198621@naver.com>
2022-12-10 02:25:37 +0900

    자동화 배포 테스트

--- src/main/java/study/springaws/SpringAwsApplication.java
+++ src/main/java/study/springaws/SpringAwsApplication.java
@@ -3,6 +3,7 @@ package study.springaws;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +17,18 @@ import javax.persistence.EntityManager;
 @EnableCaching
 public class SpringAwsApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringAwsApplication.class);
         application.addListeners(new ApplicationPidFileWriter());
         application.run(args);
+
+        new SpringApplicationBuilder(SpringAwsApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
     @Bean
